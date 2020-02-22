35ea35c98a40e319f6021c1f65e6211c4ad7ff6f
bmw1042 <bmw1042@wildcats.unh.edu>
2020-02-22 15:22:39 -0500

    test

--- votesmarter/lib/main.dart
+++ votesmarter/lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
       ),
       home: HomeScreen(),
