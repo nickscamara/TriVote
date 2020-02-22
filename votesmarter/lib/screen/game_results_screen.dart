7d1f014a6cdc5d76846cc69ecff1dd4e8aba39a9
bmw1042 <bmw1042@wildcats.unh.edu>
2020-02-22 18:04:22 -0500

    finished v1 of game results screen

--- votesmarter/lib/screen/game_results_screen.dart
+++ votesmarter/lib/screen/game_results_screen.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:spider_chart/spider_chart.dart';
 
 class GameResultsScreen extends StatefulWidget {
+  
   // DUMMY VALUES,
-  final ticks = [5, 10, 15];
-  final features = ["Civics", "Issues", "Candidates"];
-  final data = [5.0, 7.0, 3.0];
+  final ticks = [1, 5, 10];
+  final features = ["Civics", "Issues", "Candidates", "Wildcard"];
+  final data = [4.0, 6.0, 10.0, 5.0];
+  final score = 197;
+  final informed = "well informed.";
 
   @override
   _GameResultsScreenState createState() => _GameResultsScreenState();
@@ -15,26 +18,83 @@ class _GameResultsScreenState extends State<GameResultsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(10),
+        child: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
       body: ListView(
         children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
           Center(
-              child: Text(
-            'Game Results',
-            style: Theme.of(context).textTheme.title,
+              child: Text('Game Results', style: TextStyle(
+                  fontSize: 30,
+              ), 
           )),
           SizedBox(
-            height: 50,
+            height: 15,
           ),
+          Center(
+            child: Text('Your final score was ' + widget.score.toString(), style: TextStyle(
+              fontSize: 20,
+            ),),
+          ),
+          SizedBox(height: 50),
           Center(
             child: Container(
               child: SpiderChart(
-                labels: widget.features,
-                data: widget.data, colors: <Color>[
-                  Colors.red,
-                  Colors.blue,
-                  Colors.green,
-                ], maxValue: 20
+                  labels: widget.features,
+                  data: widget.data,
+                  colors: <Color>[
+                    Colors.red,
+                    Colors.blue,
+                    Colors.green,
+                    Colors.yellow
+                  ],
+                  maxValue: 10),
+            ),
+          ),
+          SizedBox(height: 50),
+          Center(
+            child: Text('You are ' + widget.informed, style: TextStyle(
+              fontSize: 20,
+            ),),
+          ),
+          SizedBox(height: 15),
+          Center(
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.thumb_up),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.thumb_down),
+                    onPressed: () {},
+                  )
+                ],
               ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: RaisedButton(
+              onPressed: () {
+                // RETURN HOME
+              },
+              child: Text('return to home'),
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              color: Colors.orange,
+
             ),
           ),
         ],
